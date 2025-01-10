f4fed53e593213b22361c8a864486f545b80d4d8
The Cool Guy <75526391+LUKTALL@users.noreply.github.com>
2025-01-10 11:48:34 +0100

    Changed order of E13 questions

--- Exercises_13-14/Exercise_13/Exercise_13.ino
+++ Exercises_13-14/Exercise_13/Exercise_13.ino
@@ -3,20 +3,20 @@
  * The address of the data that the pointer is pointing to
  * 
  * 13b)
+ * The code runs through the array, while printing the address of the data in HEX, and
+ * the value of the data. This is done through a pointer, getting the array values with *pointer.
+ */
+
+/*
+ * 13c)
  * After the first five iterations, the pointer exceeds the values defined in the array,
  * and just continues to the next address in memory
  */
  
-/* 16c)
+/* 16d)
   int var = 2;
   int *pointer = &var;
   *pointer = 3;
- */
-
-/*
- * 13d)
- * The code runs through the array, while printing the address of the data in HEX, and
- * the value of the data. This is done through a pointer, getting the array values with *pointer.
  */
 
 // 13e)
